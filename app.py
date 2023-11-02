d246ac82e7c1a66f2a3bf2273ca86e1be92b9e80
Gage Hilyard <gagehilyard@gmail.com>
2023-11-02 12:56:15 -0600

    All working before htmx

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 
-from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from sqlalchemy import select
 from wtforms import StringField
 from wtforms.validators import InputRequired, Length
 
@@ -23,16 +22,69 @@ def index():
     return redirect("/todos")
 
 
-@app.route("/todos", methods=["GET", "POST"])
+@app.route("/todos", methods=["GET"])
 def todo():
-    todos = Todo.query.all()
+    search = request.args.get("q")
+    if search is not None:
+        todos_set = Todo.query.filter(Todo.content.like("%" + search + "%")).all()
+    else:
+        todos_set = Todo.query.all()
+    return render_template("index.html", todos=todos_set)
+
+
+@app.route("/todos/<todo_id>", methods=["GET"])
+def todos_view(todo_id=0):
+    todo = Todo.query.filter_by(id=todo_id).first_or_404()
+    return render_template("show.html", todo=todo)
+
+
+@app.route("/todos/<todo_id>/edit", methods=["GET"])
+def todos_edit_get(todo_id=0):
+    form = EditTodo()
+    todo = Todo.query.filter_by(id=todo_id).first_or_404()
+    return render_template("edit.html", todo=todo, form=form)
+
+
+@app.route("/todos/<todo_id>/edit", methods=["POST"])
+def todos_edit_post(todo_id=0):
+    form = EditTodo()
+    todo = Todo.query.filter_by(id=todo_id).first_or_404()
+    if form.validate_on_submit():
+        todo.content = form.todo.data
+        todo.due = form.due.data
+        db.session.commit()
+        flash("Edited TODO!")
+        return redirect(url_for("todos_view", todo_id=todo.id))
+    else:
+        return render_template("edit.html", todo=todo, form=form)
+
+
+@app.route("/todos/new", methods=["GET"])
+def todo_new_get():
+    form = CreateTodo()
+    return render_template("new.html", form=form)
+
+
+@app.route("/todos/new", methods=["POST"])
+def todo_new():
     form = CreateTodo()
     if form.validate_on_submit():
-        todo = Todo(content=form.todo.data)
+        todo = Todo(content=form.todo.data, due=form.due.data)
         db.session.add(todo)
         db.session.commit()
+        flash("Created new TODO!")
         return redirect(url_for("index"))
-    return render_template("index.html", form=form, todos=todos)
+    return render_template("new.html", form=form)
+
+
+@app.route("/todos/<todo_id>/delete", methods=["POST"])
+def todos_delete(todo_id=0):
+    todo = Todo.query.filter_by(id=todo_id).first_or_404()
+    if todo is not None:
+        db.session.delete(todo)
+        db.session.commit()
+        flash("Deleted TODO!")
+    return redirect(url_for("index"))
 
 
 class Todo(db.Model):
@@ -40,10 +92,17 @@ class Todo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(100), nullable=False)
+    due = db.Column(db.String(100), nullable=False)
 
 
 class CreateTodo(FlaskForm):
     todo = StringField("Todo", validators=[InputRequired(), Length(min=1, max=100)])
+    due = StringField("Due", validators=[InputRequired(), Length(min=1, max=100)])
+
+
+class EditTodo(FlaskForm):
+    todo = StringField("Todo", validators=[InputRequired(), Length(min=1, max=100)])
+    due = StringField("Due", validators=[InputRequired(), Length(min=1, max=100)])
 
 
 if __name__ == "__main__":
