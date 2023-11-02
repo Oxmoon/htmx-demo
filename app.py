10fade47efd9232cd4ddc1d95daa486b25a4b39d
Gage Hilyard <gagehilyard@gmail.com>
2023-11-01 19:36:32 -0600

    sqlalchemy working

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from sqlalchemy import select
 from wtforms import StringField
 from wtforms.validators import InputRequired, Length
 
@@ -19,18 +20,19 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return redirect("/todo")
+    return redirect("/todos")
 
 
-@app.route("/todo", methods=["GET", "POST"])
+@app.route("/todos", methods=["GET", "POST"])
 def todo():
+    todos = Todo.query.all()
     form = CreateTodo()
     if form.validate_on_submit():
         todo = Todo(content=form.todo.data)
         db.session.add(todo)
         db.session.commit()
         return redirect(url_for("index"))
-    return render_template("index.html", form=form)
+    return render_template("index.html", form=form, todos=todos)
 
 
 class Todo(db.Model):
